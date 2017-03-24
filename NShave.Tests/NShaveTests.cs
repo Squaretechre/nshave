9d08b4809e5a94053b8813850cc083fc9b05dfe3
Dan <=>
2017-03-24 03:56:36 +0000

    mustache boolean to razor if

--- NShave.Tests/NShaveTests.cs
+++ NShave.Tests/NShaveTests.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
 namespace NShave.Tests
@@ -10,24 +12,37 @@ namespace NShave.Tests
     [TestFixture]
     public class NShaveTests
     {
+        public const string DataTemplate =
+            @"{
+  ""header"": ""Colors"",
+  ""items"": [
+      {""name"": ""red"", ""first"": true, ""url"": ""#Red""},
+      {""name"": ""green"", ""link"": true, ""url"": ""#Green""},
+      {""name"": ""blue"", ""link"": true, ""url"": ""#Blue""}
+  ],
+  ""empty"": false
+}";
+
         [TestCase]
-        public void ConvertMustacheIfStatementToRazorIfStatement()
+        public void ConvertMustacheBooleanStatementToRazorIfStatement()
         {
             const string mustache = 
-@"{{#items}}
+@"{{#empty}}
     <p>hello, world!</p>
-{{/items}}";
+{{/empty}}";
 
             const string razor = 
-@"if (@Model.items) {
+@"if (@Model.empty) {
     <p>hello, world!</p>
 }";
 
-            var convertedMustache = MustacheToRazor(mustache);
+            var dataModel = (JObject) JsonConvert.DeserializeObject(DataTemplate);
+
+            var convertedMustache = MustacheToRazor(mustache, dataModel);
             Assert.That(convertedMustache, Is.EqualTo(razor));
         }
 
-        private static string MustacheToRazor(string mustacheTemplate)
+        private static string MustacheToRazor(string mustacheTemplate, JObject dataModel)
         {
             var razorTemplate = new StringBuilder();
             using (var reader = new StringReader(mustacheTemplate))
@@ -39,16 +54,22 @@ namespace NShave.Tests
                     if (match.Success)
                     {
                         var mustacheToken = match.Groups[1].Value;
+                        var propertyName = mustacheToken.Substring(1, mustacheToken.Length - 1);
 
-                        switch (mustacheToken.First())
+                        if (mustacheToken.First() == '#')
                         {
-                            case '#':
-                                var propertyName = mustacheToken.Substring(1, mustacheToken.Length - 1);
-                                line = $"if (@Model.{propertyName}) {{";
-                                break;
-                            case '/':
-                                line = "}";
-                                break;
+                            var propertyType = dataModel[propertyName].Type;
+
+                            switch (propertyType)
+                            {
+                                case JTokenType.Boolean:
+                                    line = $"if (@Model.{propertyName}) {{";
+                                    break;
+                            }
+                        }
+                        else if (mustacheToken.First() == '/')
+                        {
+                            line = "}";
                         }
                     }
 
